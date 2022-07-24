fd46f75962f509dd6d3c387636ae2c2adc05561a
shikharbhardwaj <shikharbhardwaj68@gmail.com>
2022-07-24 17:13:00 +0530

    Sync latest changes in PopOS install

---
+++ gnome-terminal/export.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+dconf dump /org/gnome/terminal/legacy/profiles:/:4d392367-a101-4650-b835-78e6f505b421/ > profile.dconf
+
---
+++ gnome-terminal/import.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+dconf load
+/org/gnome/terminal/legacy/profiles:/:4d392367-a101-4650-b835-78e6f505b421/ < profile.dconf
