fb7d5dc6dc71446100b65a7354ac0f11284af6f4
shikharbhardwaj <8502456+shikharbhardwaj@users.noreply.github.com>
2026-08-17 22:55:49 +0200

    Suppress the fish welcome greeting

---
+++ fish/functions/fish_greeting.fish
@@ -0,0 +1,4 @@
+# Suppress the default "Welcome to fish, the friendly interactive shell"
+# greeting. Defined as an empty function so nothing is printed on startup.
+function fish_greeting --description 'Suppress the fish welcome message'
+end
